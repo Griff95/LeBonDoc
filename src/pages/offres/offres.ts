34b42d321c70c93a4b3a1339baf78528bb49b7b8
Lucas <lucas-lamy@hotmail.fr>
2019-12-11 22:02:15 +0100

    ajout bdd

--- src/pages/offres/offres.ts
+++ src/pages/offres/offres.ts
@@ -1,5 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import { ModalController } from 'ionic-angular';
+import {LoadingController, ModalController, ToastController} from 'ionic-angular';
 import { OffreSimplePage } from './offre-simple/offre-simple';
 import {Offre} from "../../models/Offre";
 import {OffresService} from "../../services/offres.service";
@@ -14,7 +14,7 @@ export class OffresPage implements OnInit, OnDestroy{
   offresList: Offre[];
   offreSubscription: Subscription;
 
-  constructor(private modalCtrl: ModalController,private offresService: OffresService) {
+  constructor(private modalCtrl: ModalController,private offresService: OffresService, private toastCtrl: ToastController, private loadingCtrl: LoadingController) {
 
   }
 
@@ -31,6 +31,59 @@ export class OffresPage implements OnInit, OnDestroy{
     let modal = this.modalCtrl.create(OffreSimplePage, {offre: offre});
     modal.present();
   }
+
+  onSaveList(){
+    let loader = this.loadingCtrl.create({
+      content: "Sauvegarde en cours..."
+    });
+    loader.present();
+    this.offresService.saveData().then(
+      ()=>{
+        loader.dismiss();
+        this.toastCtrl.create({
+          message: "Données sauvegardées !",
+          duration: 3000,
+          position: "bottom"
+          }).present();
+      }
+    ).catch(
+      (error)=>{
+        loader.dismiss();
+        this.toastCtrl.create({
+          message: error,
+          duration: 3000,
+          position: "bottom"
+        }).present();
+      }
+    )
+  }
+
+  onFetchList(){
+    let loader = this.loadingCtrl.create({
+      content: "Récupération en cours..."
+    });
+    loader.present();
+    this.offresService.retrieveData().then(
+      ()=>{
+        loader.dismiss();
+        this.toastCtrl.create({
+          message: "Données récupérées !",
+          duration: 3000,
+          position: "bottom"
+        }).present();
+      }
+    ).catch(
+      (error)=>{
+        loader.dismiss();
+        this.toastCtrl.create({
+          message: error,
+          duration: 3000,
+          position: "bottom"
+        }).present();
+      }
+    )
+  }
+
   ngOnDestroy(){
     this.offresService.offres$.unsubscribe();
   }
--- src/services/offres.service.ts
+++ src/services/offres.service.ts
@@ -1,6 +1,9 @@
 import {Offre} from "../models/Offre";
 import {Subject} from "rxjs";
 
+import * as firebase from "firebase";
+import DataSnapshot = firebase.database.DataSnapshot;
+
 export class OffresService{
 
   offres$ = new Subject<Offre[]>()
@@ -41,7 +44,35 @@ export class OffresService{
     this.offres$.next(this.offresList.slice());
   }
 
+  saveData(){
+    return new Promise((resolve, reject) => {
+      firebase.database().ref('offres').set(this.offresList).then(
+        (data: DataSnapshot) => {
+          resolve(data);
+        })
+        .catch(
+        (error) => {
+          reject(error);
+        }
+      );
+    });
+  }
 
+  retrieveData(){
+    return new Promise((resolve, reject) => {
+      firebase.database().ref('offres').once('value').then(
+        (data: DataSnapshot) => {
+          this.offresList = data.val();
+          this.emitOffres();
+          resolve('Données récuprées avec succès !')
+        })
+        .catch(
+          (error) => {
+            reject(error);
+          }
+        );
+    });
+  }
 
 }
 
