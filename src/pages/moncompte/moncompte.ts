ca764862174c588811b923437b55a8d13ee8f738
lucas <lucas-lamy@hotmail.fr>
2019-12-11 14:06:14 +0100

    correction boutton deco

--- src/pages/moncompte/moncompte.ts
+++ src/pages/moncompte/moncompte.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { ConnexionService } from '../../services/connexion.service';
+import { HomePage } from '../home/home';
+
+
 
 @Component({
   selector: 'page-moncompte',
   templateUrl: 'moncompte.html'
 })
+
 export class MonComptePage {
+
+  errorMessage: string;
+  
+   constructor(public navCtrl: NavController, public navParams: NavParams,private connexionService: ConnexionService) {
+  }
+
+
+  onDisconnect() {
+    this.connexionService.signOut();
+    this.navCtrl.setRoot(HomePage);
+  }
 
 }
