0bcabdbe2be82fc28c8c4d657142cc1270d1b5b3
Pierre Côte <cotepierre@eisti.eu>
2020-04-20 20:10:18 +0200

    startOrGetChat and sendMessage

--- back/routes/adChat.js
+++ back/routes/adChat.js
@@ -4,10 +4,9 @@ const auth = require('../middleware/auth');
 const adChatCtrl = require('../controllers/adChat');
 
 
-router.post('/:idAd', auth, adChatCtrl.startChat);
+router.post('/:idAd', auth, adChatCtrl.startOrGetChat);
 router.get('/:idChat', auth, adChatCtrl.sendMessage);
 // POST	api/chat/:idAd						--> Démarrer un chat avec un annonceur
 // PUT 	api/chat/:idChat					--> Envoyer un message dans un chat (verif que idChat inclut dans user.chats)
-
 
 module.exports = router;
