b25c0f42133435feee27b42474101952976a1377
Pierre Côte <cotepierre@eisti.eu>
2020-04-26 17:28:25 +0200

    Modification des routes AdChats

--- back/controllers/adChat.js
+++ back/controllers/adChat.js
@@ -6,31 +6,43 @@ exports.startOrGetChat = (req, res, next) => {
     console.log("startOrGetChat");
     User.findById(req.body.userId).populate("chats").then((user) => {
         for (chat in user.chats) {
-            if (chat.ad.equals(req.param.idAd)) {
+            if (chat.ad.equals(req.params.idAd)) {
                 res.status(201).json(chat);
             }
         }
-        Ad.findById(req.param.idAd).then( (ad) => {
+        Ad.findById(req.params.idAd).then( (ad) => {
             const adChat = new AdChat({
                 user: req.body.userId,
                 advertiser: ad.advertiser,
                 ad: ad._id
             });
             adChat.save()
-                .then(() => res.status(201).json({ message: 'success' }))
-                .catch((error) => {res.status(400).json({error: error});})
-        }).catch((error) => {res.status(400).json({error: error});});
-    }).catch((error) => {res.status(400).json({error: error});});
+                .then(() => res.status(201).json(adChat))
+                .catch((error) => {res.status(400).json(error)})
+        }).catch((error) => {res.status(400).json(error)});
+    }).catch((error) => {res.status(400).json(error)});
 };
 
 exports.sendMessage = (req, res, next) => {
     console.log("sendMessage");
-    const chatMsg = {
-        user: req.body.userId,
-        msg: req.body.msg,
-        createdAt: Date()
-    };
-    AdChat.findByIdAndUpdate(req.param.idChat, { $push: { msg: chatMsg }}).then((adChat) => {
-        res.status(200).json(adChat);
-    }).catch( (err) => { res.status(400).json({ error: "send message failed" })});
+    User.findById(req.body.userId).then((user) => {
+        if (user.chats.includes(req.body.idChat)) {
+            const chatMsg = {
+                user: req.body.userId,
+                msg: req.body.msg,
+                createdAt: Date()
+            };
+            AdChat.findByIdAndUpdate(req.body.idChat, { $push: { msg: chatMsg }}).then((adChat) => {
+                res.status(200).json(adChat);
+            }).catch( (error) => { res.status(400).json(error)});
+        }
+    });
+
+};
+
+exports.getUserAdChats = (req, res, next) => {
+    console.log("getUserAdChats");
+    User.findById(req.body.userId).populate("chats").then( (user) => {
+        res.status(201).json(user.chats);
+    }).catch((error) => {res.status(400).json(error)});
 };
--- back/routes/adChat.js
+++ back/routes/adChat.js
@@ -4,9 +4,8 @@ const auth = require('../middleware/auth');
 const adChatCtrl = require('../controllers/adChat');
 
 
-router.post('/:idAd', auth, adChatCtrl.startOrGetChat);
-router.get('/:idChat', auth, adChatCtrl.sendMessage);
-// POST	api/chat/:idAd						--> Démarrer un chat avec un annonceur
-// PUT 	api/chat/:idChat					--> Envoyer un message dans un chat (verif que idChat inclut dans user.chats)
+router.get('/startOrGetChat/:idAd', auth, adChatCtrl.startOrGetChat);
+router.post('/sendMessage', auth, adChatCtrl.sendMessage);
+router.get('/userAdChats', auth, adChatCtrl.getUserAdChats);
 
 module.exports = router;
